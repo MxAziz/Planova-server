c364cd9ab7e62fcfb3bc909452c6da1fc0adc830
MxAziz <hereabdulaziz@gmail.com>
2025-02-22 12:13:38 +0600

    added user post api and task post api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hathz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,18 +31,51 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    const userCollection = client.db("taskDB").collection("users");
+    const taskCollection = client.db("taskDB").collection("tasks");
+
+    //   user related api -------------------------------
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne({
+        ...user,
+        timeStamp: Date.now(),
+      });
+      res.send(result);
+    });
+
+    //   tasks related api -------------------------------
+    app.post("/tasks", async (req, res) => {
+      try {
+        const newTask = req.body;
+        const result = await taskCollection.insertOne(newTask);
+        res.status(201).json(result);
+      } catch (error) {
+        res.status(500).json({ message: "Error creating task", error });
+      }
+    });
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("simple CRUD is running");
+    res.send("Planova server is running");
 })
 
 app.listen(port, () => {
-    console.log(`Planova running on port: ${port}`);
+    console.log(`Planova server running on port: ${port}`);
 })
